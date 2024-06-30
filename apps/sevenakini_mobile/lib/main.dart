70f3b68eba288a708090410d49cd31a7b4720df9
mohamed abderraouf douadi <me.is.intik@gmail.com>
2024-06-30 14:15:22 +0100

    - mobile app created onboarding and added packages

--- apps/sevenakini_mobile/lib/main.dart
+++ apps/sevenakini_mobile/lib/main.dart
@@ -1,20 +1,26 @@
+import 'dart:io';
+
+import 'package:device_preview_screenshot/device_preview_screenshot.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:sevenakini_mobile/features/app.dart';
 
-void main() {
-  runApp(const MainApp());
-}
+void main() async {
+  final appDir = await getApplicationDocumentsDirectory();
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+  runApp(
+    ProviderScope(
+      child: DevicePreview(
+        enabled: Platform.isWindows,
+        tools: [
+          ...DevicePreview.defaultTools,
+          DevicePreviewScreenshot(
+            onScreenshot: screenshotAsFiles(appDir),
+          ),
+        ],
+        builder: (context) => const Sevenakini(),
       ),
-    );
-  }
+    ),
+  );
 }
